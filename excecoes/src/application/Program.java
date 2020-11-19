9bab01de7f4c110321972ea04b1cbbc50ab8ab10
Jefferson Augusto <72956747+Jeff-Aug@users.noreply.github.com>
2020-11-19 18:05:58 -0300

    tratando exceçoes da forma ruim

---
+++ excecoes/src/application/Program.java
@@ -0,0 +1,69 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import model.entities.Reservation;
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException {
+		Scanner sc = new  Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");//formato da data
+
+		System.out.println("Room number: ");
+		int number = sc.nextInt();
+		
+		System.out.println("Check-in date (dd/mm/yyyy)");
+		Date checkin = sdf.parse(sc.next());
+		
+		System.out.println("Checkout date (dd/mm/yyyy)");
+		Date checkout = sdf.parse(sc.next());
+		
+		
+		if(!checkout.after(checkin)) {
+			System.out.println("Error in reservation: Check-out date must be after check-in date");
+		}else {
+			Reservation reservation = new Reservation(number, checkin, checkout);
+			System.out.println(reservation.toString());
+			
+			System.out.println();
+			System.out.println("Enter data to update the reservation: ");
+			
+			System.out.println("Check-in date (dd/mm/yyyy)");
+			 checkin = sdf.parse(sc.next());
+			
+			 
+			 Date now = new Date();
+			 
+			 if (checkin.before(now)||checkout.before(now)) {
+				 
+				 
+				 System.out.println("Error in reservation : Check-out fate must be after check-in date");
+				 
+			 }else if (!checkout.after(checkin)) {
+				 
+				 System.out.println("Error in reservation: Check-out date must be after check-in date");
+				 
+			 }else {
+				 
+				 System.out.println("Checkout date (dd/mm/yyyy)");
+				 checkout = sdf.parse(sc.next());
+				 reservation.upadateDates(checkin, checkout);
+				 System.out.println(reservation.toString());
+			 
+			 }
+			 
+		}
+		
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
